e45669a508c7f1422db8f44594525a72de033417
minkhantko-0 <minkhant5771@gmail.com>
2024-10-03 21:56:01 +0630

    abs route

---
+++ src/constants/abs.areas.ts
@@ -0,0 +1,194 @@
+export const ABS_AREAS = [
+  {
+    id: "epigastrium",
+    position: {
+      default: {
+        top: "35.5%",
+        left: "42.5%",
+      },
+      sm: {
+        top: "36.5%",
+        left: "43%",
+      },
+    },
+    size: {
+      default: {
+        width: "12",
+        height: "12",
+      },
+      sm: {
+        width: "24",
+        height: "24",
+      },
+    },
+    label: "Epigastrium",
+  },
+  {
+    id: "llq",
+    position: {
+      default: {
+        top: "53.3%",
+        left: "52.5%",
+      },
+      sm: {
+        top: "53.3%",
+        left: "52.5%",
+      },
+    },
+    size: {
+      default: {
+        width: "12",
+        height: "12",
+      },
+      sm: {
+        width: "24",
+        height: "24",
+      },
+    },
+    label: "LLQ",
+  },
+  {
+    id: "luq",
+    position: {
+      default: {
+        top: "42.8%",
+        left: "52.5%",
+      },
+      sm: {
+        top: "42.8%",
+        left: "52%",
+      },
+    },
+    size: {
+      default: {
+        width: "12",
+        height: "12",
+      },
+      sm: {
+        width: "24",
+        height: "24",
+      },
+    },
+    label: "LUQ",
+  },
+  {
+    id: "rlq",
+    position: {
+      default: {
+        top: "53.7%",
+        left: "32%",
+      },
+      sm: {
+        top: "53.2%",
+        left: "33.8%",
+      },
+    },
+    size: {
+      default: {
+        width: "12",
+        height: "12",
+      },
+      sm: {
+        width: "24",
+        height: "24",
+      },
+    },
+    label: "RLQ",
+  },
+  {
+    id: "ruq",
+    position: {
+      default: {
+        top: "42%",
+        left: "32.5%",
+      },
+      sm: {
+        top: "42.8%",
+        left: "34%",
+      },
+    },
+    size: {
+      default: {
+        width: "12",
+        height: "12",
+      },
+      sm: {
+        width: "24",
+        height: "24",
+      },
+    },
+    label: "RUQ",
+  },
+  {
+    id: "suprapubic",
+    position: {
+      default: {
+        top: "59.5%",
+        left: "42.5%",
+      },
+      sm: {
+        top: "58.5%",
+        left: "43%",
+      },
+    },
+    size: {
+      default: {
+        width: "12",
+        height: "12",
+      },
+      sm: {
+        width: "24",
+        height: "24",
+      },
+    },
+    label: "Suprapubic",
+  },
+  {
+    id: "umbilicus",
+    position: {
+      default: {
+        top: "49%",
+        left: "43.5%",
+      },
+      sm: {
+        top: "49%",
+        left: "44%",
+      },
+    },
+    size: {
+      default: {
+        width: "10",
+        height: "10",
+      },
+      sm: {
+        width: "20",
+        height: "20",
+      },
+    },
+    label: "Umbilicus",
+  },
+  {
+    id: "all-over",
+    position: {
+      default: {
+        top: "87.2%",
+        left: "32.3%",
+      },
+      sm: {
+        top: "85%",
+        left: "34%",
+      },
+    },
+    size: {
+      default: {
+        width: "32",
+        height: "10",
+      },
+      sm: {
+        width: "64",
+        height: "20",
+      },
+    },
+    label: "All",
+  },
+];
--- src/routes/abs.tsx
+++ src/routes/abs.tsx
@@ -1,5 +1,82 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { ABS_AREAS } from "../constants/abs.areas";
 
-export const Route = createFileRoute('/abs')({
-  component: () => <div>Hello /abs!</div>,
-})
+export const Route = createFileRoute("/abs")({
+  component: AbsComponent,
+});
+
+const selectableAreas = ABS_AREAS;
+
+function AbsComponent() {
+  const [selectedAreas, setSelectedAreas] = useState<string[]>([]);
+
+  const handleClick = (areaId: string) => {
+    if (selectedAreas.includes(areaId)) {
+      // If already selected, deselect
+      if (areaId === "all-over") {
+        setSelectedAreas([]);
+      } else {
+        setSelectedAreas([
+          ...[...selectedAreas.filter((id) => id !== areaId)].filter(
+            (id) => id !== "all-over"
+          ),
+        ]);
+      }
+    } else {
+      // Add to selected areas
+      if (areaId === "all-over") {
+        setSelectedAreas([...selectableAreas.map((area) => area.id)]);
+      } else {
+        const selectedAreaArr = [...selectedAreas, areaId];
+        if (selectedAreaArr.length === selectableAreas.length - 1)
+          setSelectedAreas([...selectedAreaArr, "all-over"]);
+        else setSelectedAreas(selectedAreaArr);
+      }
+    }
+  };
+
+  return (
+    <div className="w-full flex justify-center my-12">
+      <div className="relative md:height-[50%] md:w-auto height-auto xs:w-full bg-white rounded-xl shadow-lg">
+        <img
+          onSelectCapture={(e) => e.stopPropagation()}
+          src="/assets/abdomen/default-abs.png"
+          alt="default photo for abs"
+          className="h-auto w-full "
+        />
+        {selectedAreas.map((area) => (
+          <img
+            onSelectCapture={(e) => e.stopPropagation()}
+            key={area}
+            src={`/assets/abdomen/${area}-highlight.png`}
+            alt={`${area} highlight`}
+            className="absolute top-0 left-0 "
+          />
+        ))}
+
+        {!selectedAreas.includes("all-over") &&
+          selectedAreas.map((area) => {
+            if (area !== "all-over")
+              return (
+                <img
+                  onSelectCapture={(e) => e.stopPropagation()}
+                  key={area}
+                  src={`/assets/abdomen/${area}-active.png`}
+                  alt={`${area} highlight`}
+                  className="absolute top-0 left-0 p-8"
+                />
+              );
+          })}
+
+        {selectableAreas.map((area) => (
+          <div
+            key={area.id}
+            className={`cursor-pointer rounded-full absolute sm:w-${area.size.sm.width} sm:h-${area.size.sm.height} sm:top-[${area.position.sm.top}] sm:left-[${area.position.sm.left}] w-${area.size.default.width} h-${area.size.default.height} top-[${area.position.default.top}] left-[${area.position.default.left}]`}
+            onClick={() => handleClick(area.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
